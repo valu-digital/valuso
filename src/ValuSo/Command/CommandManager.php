4c9b3db53739137d4fa1a9b7cc14740c74af2188
Juha Suni <juha.suni@mediacabinet.fi>
2013-02-13 11:21:27 +0200

    Refactored command manager; it is no longer based on EventManager

--- src/ValuSo/Command/CommandManager.php
+++ src/ValuSo/Command/CommandManager.php
@@ -2,50 +2,160 @@
 namespace ValuSo\Command;
 
 use ValuSo\Exception;
-use Zend\EventManager\EventManager;
+use ValuSo\Broker\ServiceLoader;
 use Zend\EventManager\ResponseCollection;
-use Zend\EventManager\EventInterface;
+use Zend\Stdlib\PriorityQueue;
+use \ArrayObject;
 
 /**
+ * Command manager
+ * 
  * Command manager is responsible of maintaining list of subscribed
  * command listeners and dispatching commands to correct listeners
+ * by calling their callbacks.
  * 
- * This class extends {@see \Zend\EventManager\EventManager} but does
- * not support all its features. This class is inteded to be used
- * only via {@see \ValuSo\Broker\ServiceBroker} and {@see \ValuSo\Broker\ServiceLoader} classes.
+ * The behaviour of this class is similar to {@see \Zend\EventManager\EventManager}
+ * but CommandManager does not support all of the EventManager's the features. 
+ * 
+ * This class is inteded to be used only via {@see \ValuSo\Broker\ServiceBroker} 
+ * and {@see \ValuSo\Broker\ServiceLoader} classes.
  * 
  * @author juhasuni
  */
-class CommandManager extends EventManager
+class CommandManager
 {
-    
-    protected $eventClass = 'ValuSo\Command\Command';
+    /**
+     * Service loader
+     * 
+     * @var \ValuSo\Broker\ServiceLoader
+     */
+    protected $serviceLoader = null;
     
     /**
-     * Triggers listeners for specific command
-     * 
-     * @see \Zend\EventManager\EventManager::triggerListeners()
+     * Subscribed service implementations
+     * @var array Array of PriorityQueue objects
      */
-    protected function triggerListeners($event, EventInterface $e, $callback = null)
+    protected $services = array();
+    
+    /**
+     * Retrieve service loader
+     * 
+     * @return \ValuSo\Broker\ServiceLoader
+     */
+    public function getServiceLoader()
     {
-        $command = $event;
-        $c = $e;
+        if ($this->serviceLoader === null) {
+            $this->serviceLoader = new ServiceLoader();
+        }
+        return $this->serviceLoader;
+    }
+    
+    /**
+     * Set service loader
+     * 
+     * @param ServiceLoader $serviceLoader
+     */
+    public function setServiceLoader(ServiceLoader $serviceLoader)
+    {
+        $this->serviceLoader = $serviceLoader;
+    }
+    
+    /**
+     * Attach service listener
+     * 
+     * @param string $service           Service name (case in-sensitive)
+     * @param string|Closure $callback  Callback closure or service ID
+     * @param int $priority             Listener priority (greatest priority is called first)
+     * @return LazyCallbackHandler      Listener specs
+     */
+    public function attach($service, $callback, $priority = 1)
+    {
+        $service = $this->normalizeServiceName($service);
         
+        // If we don't have a priority queue for the service yet, create one
+        if (empty($this->services[$service])) {
+            $this->services[$service] = new PriorityQueue();
+        }
+        
+        if (!is_callable($callback)) {
+            $id = $callback;
+            $callback = null;
+        } else {
+            $id = null;
+        }
+        
+        $listener = new LazyCallbackHandler(
+            $callback, 
+            array('service' => $service, 'priority' => $priority, 'serviceId' => $id));
+    
+        // Inject the callback handler into the queue
+        $this->services[$service]->insert($listener, $priority);
+        return $listener;
+    }
+    
+    /**
+     * Detach service listener
+     * 
+     * @param LazyCallbackHandler $listener
+     * @return boolean
+     */
+    public function detach(LazyCallbackHandler $listener)
+    {
+        $service = $listener->getMetadatum('service');
+        $service = $this->normalizeServiceName($service);
+        
+        if (!$service || empty($this->services[$service])) {
+            return false;
+        }
+        
+        $return = $this->services[$service]->remove($listener);
+        if (!$return) {
+            return false;
+        }
+        
+        if (!count($this->services[$service])) {
+            unset($this->services[$service]);
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Trigger all listeners for a given command
+     *
+     * Can emulate triggerUntil() if the last argument provided is a callback.
+     *
+     * @param  CommandInterface $command
+     * @param  null|callable $callback
+     * @return ResponseCollection All listener return values
+     * @throws Exception\InvalidCallbackException
+     */
+    public function trigger(CommandInterface $command, $callback = null)
+    {
+        if ($callback && !is_callable($callback)) {
+            throw new Exception\InvalidCallbackException('Invalid callback provided');
+        }
+        
+        $service = $this->normalizeServiceName($command->getService());
         $responses = new ResponseCollection();
-        $listeners = $this->getListeners($command);
+        $command->setResponses($responses);
         
-        $c->setResponses($responses);
-        
-        if ($listeners->isEmpty()) {
+        if (!isset($this->services[$service]) || $this->services[$service]->isEmpty()) {
             return $responses;
         }
         
         $exception = null;
         
-        foreach ($listeners as $listener) {
+        foreach ($this->services[$service] as $listener) {
+            
+            // Lazy load service as needed
+            if (!$listener->getCallback()) {
+                $listener->setCallback(
+                    $this->getServiceLoader()->load($listener->getMetadatum('serviceId')));
+            }
             
             try{
-                $response  = call_user_func($listener->getCallback(), $c);
+                $response  = call_user_func($listener->getCallback(), $command);
                 $exception = null;
             } catch(Exception\SkippableException $ex) {
                 
@@ -53,7 +163,7 @@ class CommandManager extends EventManager
                     $exception = $ex;
                 }
                 
-                if ($c->propagationIsStopped()) {
+                if ($command->propagationIsStopped()) {
                     $responses->setStopped(true);
                     break;
                 } else {
@@ -66,7 +176,7 @@ class CommandManager extends EventManager
             $responses->push($response);
             
             // If the event was asked to stop propagating, do so
-            if ($c->propagationIsStopped()) {
+            if ($command->propagationIsStopped()) {
                 $responses->setStopped(true);
                 break;
             }
@@ -84,5 +194,15 @@ class CommandManager extends EventManager
         }
         
         return $responses;
+    }
+    
+    /**
+     * Retrieve service name in normal form
+     *
+     * @param string $name
+     * @return string
+     */
+    public final function normalizeServiceName($name){
+        return strtolower($name);
     }
 }
