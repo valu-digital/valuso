d9814bb14fb1a768fd322257278f227f2db86fb4
Juha Suni <juha.suni@mediacabinet.fi>
2013-04-04 13:47:26 +0300

    Adds a simple service class that doesn't contain any meta information

---
+++ tests/ValuSoTest/TestAsset/SimpleService.php
@@ -0,0 +1,40 @@
+<?php
+namespace ValuSoTest\TestAsset;
+
+use ValuSo\Annotation;
+
+/**
+ * @Annotation\Version("1.0")
+ */
+class SimpleService
+{
+    public function operation1($returnValue)
+    {
+        return $returnValue;
+    }
+    
+    public function operation2($returnValue)
+    {
+        return $returnValue;
+    }
+    
+    public function operation3($returnValue)
+    {
+        return $returnValue;
+    }
+    
+    public function operation4($returnValue)
+    {
+        return $returnValue;
+    }
+    
+    public function operation5($returnValue)
+    {
+        return $returnValue;
+    }
+    
+    public function operation6($returnValue)
+    {
+        return $returnValue;
+    }
+}
