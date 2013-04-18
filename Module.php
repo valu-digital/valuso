edd9e560efc3b3eabc85de87121d7fcab7761f6a
Juha Suni <juha.suni@mediacabinet.fi>
2013-04-18 10:07:17 +0300

    Fixed docs

--- Module.php
+++ Module.php
@@ -2,6 +2,6 @@
 /**
  * This file is placed here for compatibility with Zendframework 2's ModuleManager.
  * It allows usage of this module even without composer.
- * The original Module.php is in 'src/DoctrineMongoODMModule' in order to respect PSR-0
+ * The original Module.php is in 'src/ValuSo' in order to respect PSR-0
  */
 require_once __DIR__ . '/src/ValuSo/Module.php';
