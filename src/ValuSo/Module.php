0a1de6b6d1aecf0d28ccff235b0d5bd3084adc99
Juha Suni <juha.suni@mediacabinet.fi>
2013-09-06 15:38:05 +0300

    Implements console usage interface

--- src/ValuSo/Module.php
+++ src/ValuSo/Module.php
@@ -5,10 +5,12 @@ use Zend\ModuleManager\Feature;
 use Zend\EventManager\EventInterface;
 use Zend\Loader\AutoloaderFactory;
 use Zend\Loader\StandardAutoloader;
+use Zend\Console\Adapter\AdapterInterface as Console;
 
 class Module
     implements  Feature\AutoloaderProviderInterface, 
-                Feature\ConfigProviderInterface
+                Feature\ConfigProviderInterface,
+                Feature\ConsoleUsageProviderInterface
 {
 
     /**
@@ -36,5 +38,21 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
+    }
+    
+    public function getConsoleUsage(Console $console){
+        return array(
+                // Describe available commands
+                'Using Valu services:',
+                'exec [OPTION] SERVICE OPERATION [PARAMS]' => 'Execute a service operation',
+    
+                // Describe expected parameters
+                array( 'SERVICE', 'Name of the service' ),
+                array( 'OPERATION', 'Name of the operation' ),
+                array( 'PARAMS', 'Additional parameters', "JSON formatted query string \nRemember to use single quotes to escape JSON and double quotes to escape param names. \nExample: ./valu.php exec user create '{\"username\":\"admin\"}'" ),
+                array( '--user|-u', 'Username', 'If username is omitted, it will be prompted'),
+                array( '--password|-p', 'Password', 'If password is omitted, it will be prompted'),
+                array( '--verbose|-v', 'Verbose mode', 'Display additional error information' ),
+        );
     }
 }
