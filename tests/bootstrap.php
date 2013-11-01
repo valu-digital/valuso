de9e148b7b6554b5c8062c443f3087cb9571df55
Juha Suni <juha.suni@mediacabinet.fi>
2013-11-01 13:07:46 +0200

    Adds configurations for Travis builds

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,18 @@
 <?php
 /*
-* Set error reporting to the level to which VALU code must comply.
+* Set error reporting to the level to which Valu code must comply.
 */
 error_reporting( E_ALL | E_STRICT );
 
-ini_set('display_errors', 1);
-
 // Setup autoloading
-include './_autoload.php';
+$autoloadScript = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($autoloadScript)) {
+    $autoloadScript = __DIR__ . '/../../../autoload.php';
+}
+
+if (!file_exists($autoloadScript)) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+$loader = include_once $autoloadScript;
+$loader->add('ValuSoTest', __DIR__);
