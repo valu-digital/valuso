7d8cc6b5e3a1242f96e9857d30d236c5735500b7
Juha Suni <juha.suni@mediacabinet.fi>
2014-09-23 10:54:17 +0300

    Removes useless interface

--- src/ValuSo/Feature/ServiceWrapperAwareInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ValuSo\Feature;
-
-use ValuSo\Broker\ServiceWrapper;
-
-interface ServiceWrapperAwareInterface
-{
-    public function setServiceWrapper(ServiceWrapper $serviceWrapper);
-}
